8fcb8d4ea24ba13f6bb6758b3a373c64ad70a32d
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-26 20:08:21 +0500

    Deleted: Deleted HelloApiView

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import HelloAPIView, NewsZaminUzView
+from .views import NewsZaminUzView
 
 
 urlpatterns = [
-    path('hello/', HelloAPIView.as_view(), name='hello_api_view'),
     path('', NewsZaminUzView.as_view(), name='News Api, using Zamin.uz web site'),
 ]
