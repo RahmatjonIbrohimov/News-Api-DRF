fddfae63b61fdcc9788994e6539efe86d59c7acb
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-26 18:59:31 +0500

    Add: Added NewsZaminUzView Urls, line 8

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HelloAPIView
+from .views import HelloAPIView, NewsZaminUzView
 
 
 urlpatterns = [
     path('hello/', HelloAPIView.as_view(), name='hello_api_view'),
+    path('', NewsZaminUzView.as_view(), name='News Api, using Zamin.uz web site'),
 ]
