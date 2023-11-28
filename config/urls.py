9e4b14334320b2afd694e2ffb1392dd7ee48be80
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-28 11:44:06 +0500

    Add: Added apiv2

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('', include('api.urls')),
+    path('latest', include('apiv2.urls')),
     path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='Swagger UI')
