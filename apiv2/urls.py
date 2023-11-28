e82b654e8500d5ca6d604a97dfa563d7ea26d2c8
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-28 11:52:20 +0500

    Update: Updated urlpatterns, Added NewsZaminView

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from .views import NewsZaminUzView
+
 
 urlpatterns = [
-    path(''),
+    path('', NewsZaminUzView.as_view(), name='News Api, using Zamin.uz web site'),
 ]
