fc98523c95436c41e92ca3ab35bf3db3af76afe7
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-28 11:56:05 +0500

    Add: Added NewsKeeperModel

--- apiv2/models.py
+++ apiv2/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class NewsKeeperModel(models.Model):
+    photo = models.URLField()
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    description = models.TextField()
+    date = models.CharField(max_length=12)
+
+    class Meta:
+        db_table = 'LatestNews'
