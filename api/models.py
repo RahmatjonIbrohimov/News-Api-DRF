ff663a359e53113fb0856538ce82797d2957665f
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-28 11:34:05 +0500

    Update: Updated NewsKeeperModel, database has been renamed: ... -> LatestNews

--- api/models.py
+++ api/models.py
@@ -7,3 +7,6 @@ class NewsKeeperModel(models.Model):
     url = models.URLField()
     description = models.TextField()
     date = models.CharField(max_length=12)
+
+    class Meta:
+        db_table = 'LatestNews'
