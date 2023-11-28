c6bd7db53415cb8be61a2dffc19fd3a46ad07ace
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-28 11:25:44 +0500

    Add: Add NewsKeeperModel

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsKeeperModel(models.Model):
+    photo = models.URLField()
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    description = models.TextField()
+    date = models.CharField(max_length=12)
