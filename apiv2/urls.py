02765debcbf975d812255b90242197004952d06e
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-29 17:46:11 +0500

    Add: Added filter/ path

--- apiv2/urls.py
+++ apiv2/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import NewsZaminUzView
+from .views import NewsZaminUzView, ProductList
 
 
 urlpatterns = [
     path('', NewsZaminUzView.as_view(), name='News Api, using Zamin.uz web site'),
+    path('filter/', ProductList.as_view()),
 ]
