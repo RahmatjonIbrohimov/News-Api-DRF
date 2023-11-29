24a9b151e29ac431784bc78ce01a4df78a999cc2
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-29 17:46:43 +0500

    Update: Updated date length 12 -> 11

--- apiv2/models.py
+++ apiv2/models.py
@@ -6,7 +6,7 @@ class NewsKeeperModel(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField()
     description = models.TextField()
-    date = models.CharField(max_length=12)
+    date = models.CharField(max_length=11)
 
     class Meta:
         db_table = 'LatestNewsApiV2'
