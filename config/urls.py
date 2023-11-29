1014ecefbdee18d89b3a37865e5ed63534bcabad
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-29 14:47:15 +0500

    Update: The main application is accessed via an api request

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('', include('api.urls')),
-    path('latest', include('apiv2.urls')),
+    path('api/', include('api.urls')),
+    path('latest/', include('apiv2.urls')),
     path('admin/', admin.site.urls),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='Swagger UI')
