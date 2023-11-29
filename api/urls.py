b1ca313c6110d80273c19102ca549019bd0c6b41
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-29 20:13:35 +0500

    Update: added migrations/

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import NewsZaminUzView
+from .views import NewsZaminUzView, migration
 
 
 urlpatterns = [
     path('', NewsZaminUzView.as_view(), name='News Api, using Zamin.uz web site'),
+    path('migrations/', migration, name='do migrations'),
 ]
