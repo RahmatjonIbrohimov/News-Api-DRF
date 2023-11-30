5d9f8d67d6042e7b56b315a074488d160d1172ea
Rahmatjon <rahmatjonibragimov17@gmail.com>
2023-11-30 07:33:06 +0500

    Add: added filter/

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import NewsZaminUzView, migration
+from .views import NewsZaminUzView, migration, NewsFilterView
 
 
 urlpatterns = [
     path('', NewsZaminUzView.as_view(), name='News Api, using Zamin.uz web site'),
+    path('filter/', NewsFilterView.as_view(), name='FIlter Page'),
     path('migrations/', migration, name='do migrations'),
 ]
